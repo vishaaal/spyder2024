d555446457004410e392713153ecc85078678ef4
vishaaal <officialvk02@gmail.com>
2024-02-18 13:31:54 +0530

    [feat] payments style

--- src/pages/Payments/Left.js
+++ src/pages/Payments/Left.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './Left.css'
+
 
 const Left = () => {
   return (
@@ -6,7 +8,9 @@ const Left = () => {
       <div className="text-6xl font-bold py-3 mb-4">Confirm & Pay</div>
       <div className="p-6 border-2 rounded-lg mb-4">
         <div className="font-bold">Free Cancellation</div>
-        <div className="text-slate-500">Tickets can be cancelled by 13th December, 2022</div>
+        <div className="text-slate-500">
+          Tickets can be cancelled by 13th December, 2022
+        </div>
       </div>
 
       <div className="my-8">
@@ -15,16 +19,78 @@ const Left = () => {
           We'll be sending your ticekts to the details below. Booking for a
           friend? Add their details
         </div>
-        <div></div>
+        <div >
+          <form action="/user_login.js"  />
+          <input type="text" placeholder="First Name" class="email" required />
+          <br />
+          <div className="yoo">
+            <input
+              type="text1"
+              placeholder="Country Code"
+              class="email"
+              required
+            />
+            <br />
+            <input
+              type="text1"
+              placeholder="Phone Number"
+              class="email"
+              required
+            />
+            <br />
+          </div>
+          <div className="yoo" >
+            <input type="text1" placeholder="Email" class="email" required />
+            <br />
+            <input
+              type="text1"
+              placeholder="Confirm email"
+              class="Confirm email"
+            />
+            <br />
+          </div>
+
+          <br />
+        </div>
       </div>
       <div>
-        <div className="text-2xl font-semibold py-2">Additional information</div>
-        <div className="pt-4 text-slate-500">We need a few more details to complete your reservation</div>
-        <div></div>
+        <div className="text-2xl font-semibold py-2">
+          Additional information
+        </div>
+        <div className="pt-4 text-slate-500">
+          We need a few more details to complete your reservation
+        </div>
+        <div>
+          <form action="/info.js" />
+          <div className="yoo">
+            <input
+              type="text1"
+              placeholder="Input Label"
+              class="label"
+              required
+            />
+            <br />
+            <input type="text1" placeholder="Select" class="Select" required />
+            <br />
+          </div>
+          <input
+            type="text"
+            placeholder="MultiSelect"
+            class="MultiSelect"
+            required
+          />
+          <br />
+
+          <br />
+        </div>
       </div>
       <div>
-        <div className="text-2xl font-semibold py-2">Select your mode of payment</div>
-        <div className="pt-4 text-slate-500">Payments with Tickets are secure and encrpyted</div>
+        <div className="text-2xl font-semibold py-2">
+          Select your mode of payment
+        </div>
+        <div className="pt-4 text-slate-500">
+          Payments with Tickets are secure and encrpyted
+        </div>
         <div></div>
       </div>
       <div>
--- src/pages/Payments/Right.js
+++ src/pages/Payments/Right.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import mountain from "../../assets/mountain.jpeg";
 
 const Right = () => {
   return (
-    <div>Right</div>
-  )
-}
+    <div className="m-2 p-6 rounded-lg border-2">
+      <div className="rounded-lg py-4">
+        {" "}
+        <img className="h-36 w-72 rounded-lg" src={mountain} />
+      </div>
+      <div className="py-4">4.9 4.5 category</div>
+      <div className="py-4 font-semibold">
+        {" "}
+        Amsterdam open boat canal cruise - Live Guiye - from Anne Frank Housef
+      </div>
+      <div className="py-4 font-semibold">
+        <div>ticket type - variant</div>
+        <div>day month date</div>
+        <div>time</div>
+        <div>5 guests</div>
+      </div>
+      <div className="py-4">View payment summary</div>
+      <div>
+        <div className="flex justify-between">
+          <div className="font-semibold">Total Payable</div>
+          <div className="font-semibold">$XXX</div>
+        </div>
+        <div className="text-green-600 bg-green-200 w-1/3 rounded-lg">You saved price</div>
+        <div className="py-4">You will be charged in AED</div>
+        <div className="py-7">By clicking “confirm & pay”, you agree to Tickete’s general terms and conditions and cancellation policy.</div>
+        <div className="py-4"><button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Confirm & Pay
+          </button></div>
+      </div>
+    </div>
+  );
+};
 
-export default Right
+export default Right;
