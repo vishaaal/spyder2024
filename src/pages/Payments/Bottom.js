94b8e46d7fc6c9e74eb2a6a0330e527821fb4118
vishaaal <officialvk02@gmail.com>
2024-02-18 13:14:32 +0530

    feat Bottom.js

--- src/pages/Payments/Bottom.js
+++ src/pages/Payments/Bottom.js
@@ -3,11 +3,16 @@ import React from "react";
 const Bottom = () => {
   return (
     <div className="flex m-8">
-      <div>
+        
+
+
+      <div className="basis-1/3">
         <div className="text-2xl font-semibold py-2">
           Frequently asked questions
         </div>
-        <div className="pt-4 text-slate-500">Here are some of our most asked questions.</div>
+        <div className="pt-4 text-slate-500">
+          Here are some of our most asked questions.
+        </div>
         <div>
           <div className="mt-4 bg-slate-200 rounded-lg p-4 ">
             <div className="text-xl font-semibold py-2">
@@ -23,17 +28,27 @@ const Bottom = () => {
               </button>
             </div>
           </div>
-          <div>
-            <div>What should I do on my first trip to Rome?</div>
-            <div>What are some hidden gems to see in Rome</div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+         </div>
+         </div>
+        <div className="basis-2/3 p-6">
+          <div className="p-5 font-semibold">What should I do on my first trip to Rome?</div>
+          <div className="p-5 ">
+            <div className="font-semibold">What are some hidden gems to see in Rome</div>
+            <div>
+              Rome is packed with hidden gems. If you love art, don't miss the
+              Galleria Doria Pamphilj or Palazzo Barberini. The Museo Barracco
+              is a free museum and well worth a quick visit. If you love the
+              ancient stuff, don't miss the Domus Romana at Palazzo Valentini,
+              the Domus Aurea, and the Baths of Caracalla. For even more hidden
+              gems, take a tour of Trastevere, a fascinating neighborhood full
+              of history and ambiance.
+            </div>
+          </div >
+          <div className="p-5 font-semibold">How much time should I spend in Rome?</div>
+          <div className="p-5 font-semibold">What food is Rome known for?</div>
+          <div className="p-5 font-semibold">What is the best way to get around Rome?</div>
         </div>
       </div>
-      <div></div>
-    </div>
   );
 };
 
